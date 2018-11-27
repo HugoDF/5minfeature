c26e3cf43c858ce8bd83a5cbfb5fc444140465bc
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2018-11-27 21:18:45 +0000

    add feature name input

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,32 @@
 <template>
   <div id="app">
-    <TimeSelector
-      :activeTimeName="activeTimeName"
-      @selected="onTimeSelected($event)"
-      :disabled="countingDown"
+    <div class="container">
+      <FeatureNameInput
+        :feature-name="featureName"
+        @change="onFeatureNameChanged($event)"
+        :disabled="countingDown"
       />
-    <Countdown
-      :time="activeTime"
-      leading-zero
-      emit-events
-      :auto-start="false"
-      ref="countdown">>
-      <template slot-scope="props">
-        {{props.minutes}}:{{props.seconds}} left
-      </template>
-    </Countdown>
-    <div>
+    </div>
+    <div class="container">
+      <TimeSelector
+        :activeTimeName="activeTimeName"
+        @selected="onTimeSelected($event)"
+        :disabled="countingDown"
+        />
+    </div>
+    <div class="container">
+      <Countdown
+        :time="activeTime"
+        leading-zero
+        emit-events
+        :auto-start="false"
+        ref="countdown">>
+        <template slot-scope="props">
+          {{props.minutes}}:{{props.seconds}} left
+        </template>
+      </Countdown>
+    </div>
+    <div class="container">
       <button @click="toggleCountdown()">
         {{ countingDown ? "Stop" : "Start" }}
       </button>
@@ -24,20 +35,24 @@
 </template>
 
 <script>
-import Countdown from '@xkeshi/vue-countdown'
+import FeatureNameInput from '@/components/FeatureNameInput.vue'
 import TimeSelector from '@/components/TimeSelector.vue'
+import Countdown from '@xkeshi/vue-countdown'
+
 
 export default {
   name: 'app',
   components: {
+    FeatureNameInput,
+    TimeSelector,
     Countdown,
-    TimeSelector
   },
   data() {
     return {
       activeTimeName: undefined,
       activeTime: undefined,
-      countingDown: false
+      countingDown: false,
+      featureName: ''
     }
   },
   methods: {
@@ -47,6 +62,9 @@ export default {
     },
     toggleCountdown() {
       this.countingDown = !this.countingDown
+    },
+    onFeatureNameChanged(newName) {
+      this.featureName = newName
     }
   },
   watch: {
@@ -65,5 +83,9 @@ export default {
 #app {
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   text-align: center;
+}
+.container {
+  margin-top: 2em;
+  margin-bottom: 2em;
 }
 </style>
---
+++ src/components/FeatureNameInput.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="feature-name-container">
+    <input
+      :disabled="disabled"
+      class="feature-name-input"
+      placeholder="🆕 feature name"
+      type="text"
+      v-model="internalFeatureName"
+      />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    featureName: {
+      type: String,
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    internalFeatureName: {
+      get() {
+        return this.featureName
+      },
+      set(newName) {
+        this.$emit('change', newName)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.feature-name-container {
+  text-align: center;
+}
+.feature-name-input {
+  text-align: center;
+  font-size: 1.5em;
+  width: 70%;
+  border: none;
+  padding: .5em;
+}
+@media screen and (max-width: 640px) {
+  .feature-name-input {
+    width: 100%;
+  }
+}
+</style>
